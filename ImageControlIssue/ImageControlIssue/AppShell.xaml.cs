a01802eb88f743805bc64071c2fa4f6377d2c473
SVRagul <92798966+SVRagul@users.noreply.github.com>
2022-11-30 10:21:02 +0530

    Add files via upload

---
+++ ImageControlIssue/ImageControlIssue/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageControlIssue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ImageControlIssue/ImageControlIssue/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ImageControlIssue;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
